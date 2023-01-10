a2004b99aad94198960f69b5cb315eb9bb6dfae6
Mathis Sauvage <sauvagemathis@gmail.com>
2023-01-10 09:55:22 +0100

    Test avec le json _

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 function load_question(){
+    const nom_theme = document.getElementById('nom_theme');
     const question_name = document.getElementById('question_name');
     const response_1 = document.getElementById('response_1');
     const response_2 = document.getElementById('response_2');
     const response_3 = document.getElementById('response_3');
     const response_4 = document.getElementById('response_4');
 
-    question_name.innerHTML = "Test"
+    
 
     readJSON("jsonexample.json").then(data => { 
-        // Utilisez ici les données JSON qui ont été lues
+        nom_theme.innerHTML = data[0].nom
+        question_name.innerHTML = data[1][0].question
+        response_1.innerHTML = data[1][0].réponse1
+        response_2.innerHTML = data[1][0].réponse2
+        response_3.innerHTML = data[1][0].réponse3
+        response_4.innerHTML = data[1][0].réponse4
         console.log(data);
     }).catch(error => {
-        // Traitez ici les erreurs potentielles
         console.log(error);
     });
 
