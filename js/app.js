de9f2ab6816a72000933e48b573f2b118308c7ec
Mathis Sauvage <sauvagemathis@gmail.com>
2023-01-10 14:03:56 +0100

    Adaptation à l'interface

--- js/app.js
+++ js/app.js
@@ -1,43 +1,26 @@
-function load_question(){
-    const nom_theme = document.getElementById('nom_theme');
-    const question_name = document.getElementById('question_name');
-    const reponse_1 = document.getElementById('reponse_1');
-    const reponse_2 = document.getElementById('reponse_2');
-    const reponse_3 = document.getElementById('reponse_3');
-    const reponse_4 = document.getElementById('reponse_4'); 
-
-    readJSON("jsonexample.json").then(data => { 
-        nom_theme.innerHTML = data[0].nom
-        question_name.innerHTML = data[1][0].question
-        reponse_1.innerHTML = data[1][0].reponse1
-        reponse_2.innerHTML = data[1][0].reponse2
-        reponse_3.innerHTML = data[1][0].reponse3
-        reponse_4.innerHTML = data[1][0].reponse4
-        console.log(data);
+async function load_question(){
+    let questions;
+    await readJSON("questions/histoire-geo.json").then(data => { 
+        questions = data;
+        console.log(data);        
     }).catch(error => {
         console.log(error);
     });
-}
 
-function load_theme(){
-    const input = document.querySelector('input[type="file"]');
+    console.log(questions);
 
-    input.addEventListener('change', (event) => {
-        const files = event.target.files;
-
-        for (let i = 0; i < files.length; i++) {
-            console.log(files[i].name);
-        }
-    });
+    const question = document.getElementById('question');
+    const reponse1 = document.getElementById('reponse1');
+    const reponse2 = document.getElementById('reponse2');
+    const reponse3 = document.getElementById('reponse3');
+    const reponse4 = document.getElementById('reponse4'); 
 
 
-    const button = document.createElement('button');
-    button.textContent = 'Cliquez ici';
-    button.addEventListener('click', function() {
-        console.log('Le bouton a été cliqué');
-    });
-    const div = document.getElementById('test');
-    div.appendChild(button);
+    question.innerHTML = questions[1][0].question
+    reponse1.innerHTML = questions[1][0].reponse1
+    reponse2.innerHTML = questions[1][0].reponse2
+    reponse3.innerHTML = questions[1][0].reponse3
+    reponse4.innerHTML = questions[1][0].reponse4
 }
 
 function readJSON(file) {
--- qcm.php
+++ qcm.php
@@ -1,26 +1,46 @@
+<?php
+    $data = json_decode(file_get_contents('questions/histoire-geo.json'), true);
+    if(json_last_error() != JSON_ERROR_NONE){
+        throw new Exception("Error when decoding the json ".json_last_error_msg());
+    }    
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="js/app.js"></script>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/pally.css">
 </head>
+
+<button onClick="load_question()">Test</button>
+
 <body>
     <nav>
         <ul>
-            <a href="index.html"><li>Mathématiques</li></a>
-            <a href="histgeo.html"><li>Histoire-Géographie</li></a>
-            <a href="cg.html"><li>Culture Générale</li></a>
-            <a href="#"><li>Gérer les questions</li></a>
+            <a href="question_ouverte.html">
+                <li>Mathématiques</li>
+            </a>
+            <a href="image.html">
+                <li>Histoire-Géographie</li>
+            </a>
+            <a href="qcm.html">
+                <li>Culture Générale</li>
+            </a>
+            <a href="#">
+                <li>Gérer les questions</li>
+            </a>
         </ul>
     </nav>
 
     <div class="main">
         <div class="question">
-                <p class="quote">Avec quel objet puis-je jouer au baseball ?</p>
+            <p class="quote" id="question">Avec quel objet puis-je jouer au baseball ?</p>
         </div>
 
         <div class="choixMultiple">
@@ -31,11 +51,10 @@
             <br>
             <div class="ligne ligne2">
                 <button type="disabled" class="reponseM" id="reponse3">Des quilles</button>
-                <button type="disabled" class="reponseM" id="reponse4" >Un volant</button>
+                <button type="disabled" class="reponseM" id="reponse4">Un volant</button>
             </div>
         </div>
     </div>
-
-
 </body>
+
 </html>
