6b9792a33a5adc26e7bb683c86eec9e54fb12e47
Mathis Sauvage <sauvagemathis@gmail.com>
2023-01-11 13:38:39 +0100

    Pleins de trucs

--- qcm.php
+++ qcm.php
@@ -1,43 +1,49 @@
 <?php
-    session_start();
-    
-    //Vérifie si les questions sont déjà dans la session
-    $questions = json_decode($_SESSION['questions']);
-       
-    if(is_null($questions)){
-        //Si les questions ne sont pas dans la session, on lit le fichier JSON
-        $json = file_get_contents("questions/histoire-geo.json");
-        $data = json_decode($json, true);
-    
-        //Initialise le tableau de questions
-        $questions = array();
-            
-        // On choisit aléatoirement 3 questions parmi les données lues dans le fichier JSON
-        for($i = 0; $i < 3; $i++){
-            //Choisir un index aléatoire
-            $random_index = array_rand($data[1]);
-            
-            //ajoute la question choisie au tableau de questions
-            $questions[] = $data[1][$random_index];
-            
-            //supprime la question choisie de l'array $data[1]
-            unset($data[1][$random_index]);
-        }
-        //stocke les questions choisies dans la session
-        $_SESSION['questions'] = json_encode($questions);
-    }
+session_start();
 
-    $questions = json_decode($_SESSION['questions']);
+//Vérifie si les questions sont déjà dans la session
+$questions = json_decode($_SESSION['questions']);
 
-    $iteration = $_SESSION['iteration'];
-    if(is_null($iteration) || $iteration >= 10){
-        $_SESSION['iteration'] = 0;
-        $iteration = 0;
-    }else{
-        $iteration = $iteration + 1;
-        $_SESSION['iteration'] = $iteration;
+if (is_null($questions)) {
+    //Si les questions ne sont pas dans la session, on lit le fichier JSON
+    $json = file_get_contents("questions/histoire-geo.json");
+    $data = json_decode($json, true);
+
+    //Initialise le tableau de questions
+    $questions = array();
+
+    // On choisit aléatoirement 3 questions parmi les données lues dans le fichier JSON
+    for ($i = 0; $i < 3; $i++) {
+        //Choisir un index aléatoire
+        $random_index = array_rand($data[1]);
+
+        //ajoute la question choisie au tableau de questions
+        $questions[] = $data[1][$random_index];
+
+        //supprime la question choisie de l'array $data[1]
+        unset($data[1][$random_index]);
     }
-    echo $iteration;
+    //stocke les questions choisies dans la session
+    $_SESSION['questions'] = json_encode($questions);
+}
+
+$questions = json_decode($_SESSION['questions']);
+
+$iteration = $_SESSION['iteration'];
+if (is_null($iteration) || $iteration >= 10) {
+    $_SESSION['iteration'] = 0;
+    $iteration = 0;
+} else {
+    $iteration = $iteration + 1;
+    $_SESSION['iteration'] = $iteration;
+}
+echo $iteration;
+
+$rand = array("1", "2", "3", "4");
+shuffle($rand);
+
+print_r($questions[$iteration]);
+// echo $questions[0][1];
 ?>
 
 <!DOCTYPE html>
@@ -83,18 +89,18 @@
         <div class="choixMultiple">
             <form action="#" method="post">
                 <div class="ligne ligne1">
-                    <?php echo "<button class='reponseM' id='reponse1'>", $questions[$iteration]->reponse1, "</button>"; ?>
-                    <?php echo "<button class='reponseM' id='reponse2'>", $questions[$iteration]->reponse1, "</button>"; ?>
+                    <?php echo "<button class='reponseM' id='", $rand[0], "'>", $questions[$iteration]->reponse1, "</button>"; ?>
+                    <?php echo "<button class='reponseM' id='", $rand[1], "'>", $questions[$iteration]->reponse2, "</button>"; ?>
                 </div>
                 <br>
                 <div class="ligne ligne2">
-                    <?php echo "<button class='reponseM' id='reponse3'>", $questions[$iteration]->reponse1, "</button>"; ?>
-                    <?php echo "<button class='reponseM' id='reponse4'>", $questions[$iteration]->reponse1, "</button>"; ?>
+                    <?php echo "<button class='reponseM' id='", $rand[2], "'>", $questions[$iteration]->reponse3, "</button>"; ?>
+                    <?php echo "<button class='reponseM' id='", $rand[3], "'>", $questions[$iteration]->reponse4, "</button>"; ?>
                 </div>
                 <div class="ligne ligne3">
-                    <?php 
-                        if($iteration < 2) echo "<button type='submit' id='jeReponds'>Je réponds !</button>";
-                        else echo "<button type='submit' id='jeReponds'>Fin !</button>";
+                    <?php
+                    if ($iteration < 2) echo "<button type='submit' id='jeReponds'>Je réponds !</button>";
+                    else echo "<button type='submit' id='jeReponds'>Fin !</button>";
                     ?>
                 </div>
             </form>
