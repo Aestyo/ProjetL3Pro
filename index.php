ca24d2e876bfd66b40ed49982679f2d4f14bf2fe
Mathis Sauvage <sauvagemathis@gmail.com>
2023-03-06 09:14:15 +0100

    Amélioration css

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Projet Alternant</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/pally.css">
     <script src="js/app.js"></script>
@@ -13,37 +13,17 @@
 </head>
 
 <body>
-    <nav>
-        <ul>
-            <a href="index.html">
-                <li>Accueil</li>
-            </a>
-            <a href="math.html">
-                <li>Mathématiques</li>
-            </a>
-            <a href="start.html">
-                <li>Histoire-Géographie</li>
-            </a>
-            <a href="question_ouverte.html">
-                <li>Culture Générale</li>
-            </a>
-            <a href="#">
-                <li>Gérer les questions</li>
-            </a>
-        </ul>
-    </nav>
 
-    <form>
-        <button type="submit" id="jeReponds" formaction="index.html">Accueil</button>
-    </form>
+    <h1 class="enonce">Choisissez votre catégorie</h1>
+
     <form>
         <button type="submit" id="jeReponds" formaction="math.html">Mathématiques</button>
     </form>
     <form>
-        <button type="submit" id="jeReponds" formaction="start.php">Histoire-Géographie</button>
+        <button type="submit" id="jeReponds" formaction="start.php">Histoire-Géo</button>
     </form>
     <form>
-        <button type="submit" id="jeReponds" formaction="question_ouverte.html">Culture Générale</button>
+        <button type="submit" id="jeReponds" formaction="question_ouverte.html">Culture G</button>
     </form>
 
 </body>
